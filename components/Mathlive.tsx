dfafa8d10d4c5a8a461a99605313f5e5f03edaaf
Petri Montonen <91892495+PJM178@users.noreply.github.com>
2023-05-14 10:40:49 +0300

    preliminary working mathfield component

---
+++ components/Mathlive.tsx
@@ -0,0 +1,86 @@
+import * as React from 'react';
+
+import { CSSProperties, useEffect, useMemo } from 'react';
+import { MathfieldElement, MathfieldOptions } from 'mathlive';
+import { create } from 'jss';
+import camelCase from 'jss-plugin-camel-case';
+
+// mathfield configs
+MathfieldElement.fontsDirectory = '/assets/mathlive/fonts'
+MathfieldElement.soundsDirectory = '/assets/mathlive/sounds'
+
+export interface MathEditorProps {
+  options?: Partial<MathfieldOptions>;
+
+  value: string;
+  onChange: (latex: string) => void;
+  onPlaceholderChange?: (placeholderId: string, latex: string) => void;
+
+  className?: string;
+  containerStyle?: CSSProperties;
+  placeholderStyle?: CSSProperties;
+};
+
+/**
+ * @returns a styled math-editor as a non-controlled React component with placeholder support.
+ */
+const MathEditor = (props: MathEditorProps) => {
+  const containerRef = React.useRef<HTMLDivElement>(null);
+  const mfe = useMemo(() => new MathfieldElement(props.options), []);
+
+  useEffect(() => {
+    const container = containerRef.current!!;
+    container.innerHTML = "";
+    container.appendChild(mfe);
+
+    mfe.className = props.className || "";
+
+    // Listen to changes to main mathfield
+    mfe.addEventListener("input", ({ target }) =>
+      props.onChange((target as HTMLInputElement).value || "")
+    );
+
+    // // Listen to placeholders, firing if onPlaceholderChange present
+    // mfe.addEventListener("placeholder-change", ({ detail }) => {
+    //   const { placeholderId } = detail;
+    //   const value = mfe.getPlaceholderField(placeholderId)?.getValue() || "";
+    //   if (props.onPlaceholderChange) {
+    //     props.onPlaceholderChange(placeholderId, value);
+    //   }
+    // });
+
+    // // Add custom styles to placeholder mathfield elements
+    // Object.values(mfe.placeholders).forEach((placeholder) =>
+    //   addStyleEl(placeholder, props.placeholderStyle ?? {})
+    // );
+  }, []);
+
+  useEffect(() => {
+    mfe.value = props.value;
+  }, [props.value]);
+
+  return <div ref={containerRef} style={props.containerStyle} />;
+};
+
+/**
+ * Mathlive uses shadow DOMs which don't inherit global styles.
+ * We patch this by creating <style> tags for nested mathlive
+ * elements. `jss` is used to translate React CSSProperties into
+ * a stylesheet string which is inserted in a new style node.
+ * @param el Mathfield element to create styles for
+ * @param st CSS Properties
+ */
+const addStyleEl = (el: MathfieldElement, css: CSSProperties) => {
+  const node = document.createElement("style");
+  node.innerHTML = stylesheet("placeholder-mathfield", css);
+  el.appendChild(node);
+  el.classList.add("placeholder-mathfield");
+};
+
+const jss = create({ plugins: [camelCase()] });
+const stylesheet = (className: string, styles?: CSSProperties): string =>
+  jss
+    .createStyleSheet({ [className]: styles }, { generateId: ({ key }) => key })
+    .toString();
+
+export default MathEditor;
--- pages/mathlive/index.tsx
+++ pages/mathlive/index.tsx
@@ -1,7 +1,16 @@
+import { useState } from 'react';
+
+import MathEditor from '../../components/Mathlive';
+
 const MathliveTestPage = () => {
+  const [value, setValue] = useState<string>('');
+
   return (
     <main>
       <h1>Hello</h1>
+      <div>This value comes from MathEditor onChange event:</div>
+      <div>{value}</div>
+      <MathEditor value={value} onChange={setValue} />
     </main>
   );
 };
